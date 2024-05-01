43aadc1b140fc7fb3b9c9298e717c0e1a9a9b32c
dinhhieu2003 <vuongdinhhieu2003@gmail.com>
2024-05-01 23:05:57 +0700

    remove: f file

--- f.py
+++
@@ -1,9 +0,0 @@
-from PIL import Image, ImageTk
-import cv2
-
-image = Image.open('Image/test_08-Copy.jpg')
-print(type(image))
-
-img = cv2.imread('Image/test_08-Copy.jpg')
-img_2 = Image.fromarray(img)
-print(type(img_2))
